341c7ceef87e5875121068aa2a28467ef2de23dc
saikrishna0925 <saikrishna5450@gmail.com>
2026-09-03 01:42:44 +0530

    Changes made in ET-2

--- src/main/java/com/example/expenseTracker/config/SecurityConfig.java
+++ src/main/java/com/example/expenseTracker/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.UserDetailsPasswordService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
